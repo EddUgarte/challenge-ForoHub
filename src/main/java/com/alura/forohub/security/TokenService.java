5dfa29aa8f4d117260d1923031d268fe515ca214
EddUgarte <edd.ugarte7@gmail.com>
2024-07-14 13:47:16 -0600

    Septimo commit Autenticacion y Autorizacion JWT listos

--- src/main/java/com/alura/forohub/security/TokenService.java
+++ src/main/java/com/alura/forohub/security/TokenService.java
@@ -4,6 +4,8 @@ import com.alura.forohub.entity.Autor;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -33,6 +35,27 @@ public class TokenService {
 
     private Instant generarFechaExpiracion() {
         return LocalDateTime.now().plusHours(3).toInstant(ZoneOffset.of("-05:00"));
+    }
+
+    public String getSubject(String token) {
+        if (token == null) {
+            throw new RuntimeException();
+        }
+        DecodedJWT verifier = null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret); // validando firma
+            verifier = JWT.require(algorithm)
+                    .withIssuer("forohub")
+                    .build()
+                    .verify(token);
+            verifier.getSubject();
+        } catch (JWTVerificationException exception) {
+            System.out.println(exception.toString());
+        }
+        if (verifier.getSubject() == null) {
+            throw new RuntimeException("Verifier invalido");
+        }
+        return verifier.getSubject();
     }
 
 }
